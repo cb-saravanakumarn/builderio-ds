1237ae10a287e78dd4020e1e5cb40721c9f01dcf
cb-jayaraje <jayaraj.e@chargebee.com>
2024-05-09 13:07:43 +0530

    Update new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 
-  content: [
-      // Paths to all of your components' files
-      "./packages/react/sting/src/components/**/*.{ts,tsx,ts,tsx}",
-      // Any other packages you have
-  ],
-  prefix: "s-",
-  theme: {
-      extend: {},
-  },
-  plugins: [
-      require('@chargebee/cb-sting')
-      //require('./../../stingcss')
-  ],
+    content: [
+        // Paths to all of your components' files
+        "./packages/react/sting/src/components/**/*.{ts,tsx,ts,tsx}",
+        // Any other packages you have
+    ],
+    //  prefix: "s-",
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@chargebee/cb-sting')
+        //require('./../../stingcss')
+    ],
 };
