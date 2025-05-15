641bc1a01a0a6ca959db09770f90275018bcd2dc
cb-vikram <vikram.hegde@chargebee.com>
2025-05-15 10:37:46 +0530

    package json changes

--- packages/sting-react/src/components/SBadge/constants.ts
+++ packages/sting-react/src/components/SBadge/constants.ts
@@ -1,95 +1,22 @@
 import { tv, VariantProps } from "tailwind-variants";
+import "./SBadge.css";
 
 export const badgeVariants = tv({
-  base: "s-inline-flex s-gap-1 s-items-center s-text-sm s-leading-none s-border s-border-solid s-box-border",
+  base: "badge",
   variants: {
     variant: {
-      primary: "",
-      neutral: "",
-      danger: "",
-      warning: "",
-      success: "",
-      info: "",
-      brand: "",
+      primary: "badge-primary",
+      neutral: "badge-neutral",
+      danger: "badge-danger",
+      warning: "badge-warning",
+      success: "badge-success",
+      info: "badge-info",
+      brand: "badge-brand",
     },
-    size: { regular: "s-py-1 s-px-2", large: "s-py-2 s-px-2.5" },
-    mode: { light: "", dark: "" },
-    rounded: { small: "s-rounded", full: "s-rounded-full" },
+    size: { regular: "badge-regular", large: "badge-large" },
+    mode: { light: "", dark: "badge-dark" },
+    rounded: { small: "radius-small", full: "radius-full" },
   },
-  compoundVariants: [
-    {
-      variant: "primary",
-      mode: "light",
-      className: "s-bg-primary-50 s-text-primary-600 s-border-primary-100",
-    },
-    {
-      variant: "primary",
-      mode: "dark",
-      className: "s-bg-primary-500 s-text-white s-border-primary-500",
-    },
-    {
-      variant: "neutral",
-      mode: "light",
-      className: "s-bg-neutral-50 s-text-light s-border-neutral-100",
-    },
-    {
-      variant: "neutral",
-      mode: "dark",
-      className: "s-bg-neutral-500 s-text-white s-border-neutral-500",
-    },
-    {
-      variant: "danger",
-      mode: "light",
-      className: "s-bg-red-50 s-text-red-500 s-border-red-100",
-    },
-    {
-      variant: "danger",
-      mode: "dark",
-      className: "s-bg-red-500 s-text-white s-border-red-500",
-    },
-    {
-      variant: "warning",
-      mode: "light",
-      className: "s-bg-yellow-50 s-text-yellow-900 s-border-yellow-100",
-    },
-    {
-      variant: "warning",
-      mode: "dark",
-      className: "s-bg-yellow-700 s-text-white s-border-yellow-700",
-    },
-    {
-      variant: "success",
-      mode: "light",
-      className: "s-bg-green-50 s-text-green-600 s-border-green-100",
-    },
-    {
-      variant: "success",
-      mode: "dark",
-      className: "s-bg-green-500 s-text-white s-border-green-500",
-    },
-    {
-      variant: "info",
-      mode: "light",
-      className: "s-bg-info-50 s-text-info-700 s-border-info-100",
-    },
-    {
-      variant: "info",
-      mode: "dark",
-      className: "s-bg-info-600 s-text-white s-border-info-600",
-    },
-    {
-      variant: "brand",
-      mode: "light",
-      className:
-        "s-bg-brand-deep-light s-text-brand-deep-dark s-border-brand-deep-light",
-    },
-    {
-      variant: "brand",
-      mode: "dark",
-      className:
-        "s-bg-brand-deep-dark s-text-brand-deep-light s-border-brand-deep-dark",
-    },
-  ],
   defaultVariants: {
     variant: "primary",
     mode: "light",
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("./packages/stingcss/theming/colors");
+const spacing = require("./packages/stingcss/theming/spacing");
+
+function generateColorsUtility(colorsObj) {
+  const utilities = {};
+  for (const [key, value] of Object.entries(colorsObj)) {
+    for (const [shade, hexValue] of Object.entries(value)) {
+      const name = `${key}-${shade}`;
+      utilities[name] = hexValue;
+    }
+  }
+  return utilities;
+}
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./packages/sting-react/src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  prefix: "s-",
+  // prefix: "s-",
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        ...generateColorsUtility(colors),
+        lightest: colors.neutral[500],
+        light: colors.neutral[600],
+        regular: colors.neutral[700],
+        dark: colors.neutral[800],
+        darkest: colors.neutral[900],
+      },
+      height: {
+        xsmall: "1.25rem",
+        small: "1.75rem",
+        regular: "2rem",
+        large: "2.5rem",
+      },
+      colors,
+      spacing,
+    },
   },
-  plugins: [require("./packages/stingcss/index.js")],
+  // plugins: [require("./packages/stingcss/index.js")],
 };
