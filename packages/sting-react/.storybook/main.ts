efd7202f31f86a48247acb643f05fe0ff2fbf77a
cb-vikram <vikram.hegde@chargebee.com>
2025-05-23 10:55:15 +0530

    updated storybook with additional plugins

---
+++ packages/sting-react/.storybook/main.ts
@@ -0,0 +1,55 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path, { dirname, join } from 'path';
+
+const config: StorybookConfig = {
+	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
+	addons: [
+		getAbsolutePath('@storybook/addon-links'),
+		getAbsolutePath('@storybook/addon-essentials'),
+		getAbsolutePath('@storybook/addon-onboarding'),
+		getAbsolutePath('@storybook/addon-interactions'),
+		{
+			name: '@storybook/addon-postcss',
+			options: {
+				postcssLoaderOptions: {
+					implementation: require('postcss'),
+				},
+			},
+		},
+		getAbsolutePath('@storybook/addon-a11y'),
+	],
+
+	framework: {
+		name: getAbsolutePath('@storybook/react-vite'),
+		options: {},
+	},
+
+	viteFinal: async (config) => {
+		config.plugins?.push(
+			tsconfigPaths({
+				projects: [path.resolve(path.dirname(__dirname), 'tsconfig.json')],
+			}),
+		);
+
+		// Add the alias for 'chargebee-ui'
+		config.resolve = config.resolve || {};
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'chargebee-ui': path.resolve(__dirname, '../dist'),
+			'@': path.resolve(__dirname, '../src'),
+		};
+
+		return config;
+	},
+
+	typescript: {
+		reactDocgen: 'react-docgen-typescript',
+	},
+};
+export default config;
+
+function getAbsolutePath(value: string): any {
+	return dirname(require.resolve(join(value, 'package.json')));
+}
