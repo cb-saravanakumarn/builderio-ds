9043a59177b7476e6f6abef2aed534ab7016689c
cb-vikram <vikram.hegde@chargebee.com>
2025-05-28 12:29:08 +0530

    ran prettier on sting-react

--- packages/sting-react/.storybook/preview.js
+++ packages/sting-react/.storybook/preview.js
@@ -2,14 +2,14 @@ import '../src/components/tailwind.css';
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
