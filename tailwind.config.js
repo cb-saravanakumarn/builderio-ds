ddba188fdf48eacd0db14ed88eeb1d0f519c9301
cb-vikram <vikram.hegde@chargebee.com>
2025-06-20 10:49:04 +0530

    renamed sting tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,12 @@ const stingTokens = require("./packages/sting-tokens");
 
 module.exports = {
   content: [
-    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./packages/sting-react/src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  // prefix: "s-",
   theme: {
     extend: {
-      ...stingTokens.theme.extend,
+      ...stingTokens,
     },
   },
 };
