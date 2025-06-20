3219e75c162b867e0ddbe37972b313f4874bc6da
cb-vikram <vikram.hegde@chargebee.com>
2025-06-20 11:13:44 +0530

    tokens changes

--- packages/sting-react/tailwind.config.js
+++ packages/sting-react/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {
-			...stingTokens.theme.extend,
+			...stingTokens,
 		},
 	},
 	plugins: [require('tailwindcss-animate')],
--- packages/sting-tokens/index.js
+++ packages/sting-tokens/index.js
@@ -7,11 +7,9 @@ const { fontSize, fontWeight } = require("./theme/typography");
  * @returns {Object} Tailwind configuration object that can be spread into any tailwind.config.js
  */
 module.exports = {
-  tokens: {
-    colors,
-    spacing,
-    borderRadius,
-    fontSize,
-    fontWeight,
-  },
+  colors,
+  spacing,
+  borderRadius,
+  fontSize,
+  fontWeight,
 };
